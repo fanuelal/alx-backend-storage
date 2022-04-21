b28973b8b1ca487ca4219296d0146776131c8f1c
fanuelal <fanuelalmawstop@gmail.com>
2022-04-21 01:02:39 -0700

    string fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- create table name user
 -- With these attributes: id,email,name
 
-CREATE TABLE users(
+CREATE TABLE `users`(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
 `name` VARCHAR(255));
