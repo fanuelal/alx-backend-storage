921c77086b1e4b13cd191a8c6c6e1b6de78bd46b
fanuelal <fanuelalmawstop@gmail.com>
2022-04-21 00:57:26 -0700

    unique user

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- create table name user
+-- With these attributes: id,email,name
+
+CREATE TABLE users(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+`name` VARCHAR(255));
