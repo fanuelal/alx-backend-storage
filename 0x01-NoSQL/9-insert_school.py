a8ed93b5c50ddbf9a0ba21d0b9638b7481f3ffea
fanuelal <fanuelalmawstop@gmail.com>
2022-04-26 12:19:33 -0700

    insert value

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that inser new document in mongo"""
+import mongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """function that inserts a new document
+    in a collection based"""
+    if len(kwargs) == 0:
+        return None
+    return mongo_collection.insert(kwargs)
