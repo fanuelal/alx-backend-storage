71c4749ca808b89e76cef7505ea3c9bf6593b21e
fanuelal <fanuelalmawstop@gmail.com>
2022-04-27 22:40:41 +0000

    web tracking

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,12 +8,13 @@ counter = 0
 
 def get_page(url: str) -> str:
     """track how many times a particular URL
-    was accessed in the key""" 
+    was accessed in the key"""
     redi.set(f"cached:{url}", counter)
     resp = requests.get(url)
     redi.incr(f"count:{url}")
     redi.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
     return resp.text
+
 
 if __name__ == "__main__":
     get_page('http://slowwly.robertomurray.co.uk')
